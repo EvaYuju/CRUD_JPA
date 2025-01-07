8a1ddc2b736c7e3c6e220eff07119dddba07c03f
Eva <elopezmarquez@safareyes.es>
2025-01-08 00:37:24 +0100

    persistenc controllers instanciados en controladora - logica

--- src/main/java/eva/developez/crud_jpa/logic/Controller.java
+++ src/main/java/eva/developez/crud_jpa/logic/Controller.java
@@ -1,11 +1,16 @@
 package eva.developez.crud_jpa.logic;
 
+import eva.developez.crud_jpa.persistence.Persistence_Controller;
+
 /**
  *
  * @author aquas
  */
 public class Controller {
     
+    // Logica llama a la persistencia
+    Persistence_Controller persis = new Persistence_Controller();
+       
     
     
 }
