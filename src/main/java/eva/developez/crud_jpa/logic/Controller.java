30b0c1242f80eec697ee0095b59d89aab5c8c8e6
Eva <elopezmarquez@safareyes.es>
2025-01-08 00:58:38 +0100

    llamada a guardar desde logica llamando a la persistencia v1.0

--- src/main/java/eva/developez/crud_jpa/logic/Controller.java
+++ src/main/java/eva/developez/crud_jpa/logic/Controller.java
@@ -9,7 +9,35 @@ import eva.developez.crud_jpa.persistence.Persistence_Controller;
 public class Controller {
     
     // Logica llama a la persistencia
-    Persistence_Controller persis = new Persistence_Controller();
+    Persistence_Controller controlPersis = new Persistence_Controller();
+
+    public void guardar(String nomMascota, String raza, String color, String alergico, String atEsp, String nomDuenio, String tlf, String observ) {
+
+        // Creamos el dueño y asignamos sus valores
+        Duenio duenio = new Duenio();
+        duenio.setNombreDuenio(nomDuenio);
+        duenio.setTelefono(tlf);
+        
+        // Creamos la mascota y asignamos sus valores
+        Mascota mascota = new Mascota();
+        mascota.setNombreMascota(nomMascota);
+        mascota.setRaza(raza);
+        mascota.setColor(color);
+        mascota.setAlergico(alergico);
+        mascota.setAtencion_especial(atEsp);
+        mascota.setObservaciones(observ);
+        //mascota.setNum_cliente(0); // automatica
+        
+        mascota.setUnDuenio(duenio);
+        
+        // Llamamos a la persistencia desde la logica de crear objetos en la BD
+        controlPersis.guardar(duenio,mascota);
+        
+        
+        
+        
+        
+    }
        
     
     
